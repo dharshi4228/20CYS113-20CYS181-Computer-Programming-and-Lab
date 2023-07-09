cf03079ad97e2d24efb6f0d3c0ac9190d4cd697c
DHARSHIKA S <116824635+dharshi4228@users.noreply.github.com>
2023-07-09 18:14:06 +0530

    9.7.20221[record purpose]

---
+++ generate_all_the_prime_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void generatePrimes(int start, int end) {
+    printf("Prime numbers between %d and %d are: ", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int start, end;
+    
+    scanf("%d", &start);
+    
+    scanf("%d", &end);
+
+    generatePrimes(start, end);  // calling the function to generate prime numbers
+
+    return 0;
+}
