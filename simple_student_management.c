81f8b5e1fcf877cee5260f8236a52bba87d692f6
DHARSHIKA S <116824635+dharshi4228@users.noreply.github.com>
2023-07-10 13:02:27 +0530

    10.7.2023

--- simple_student_management.c
+++ simple_student_management.c
@@ -1,129 +1,146 @@
 #include<stdio.h>
 #include<string.h>
-#define MAX_STUDENTS 3
 
-enum Scores{A,B,C,D,F};
-struct Student{
+#define MAX_STUDENTS 3
+enum score{A,B,C,D,F};
+
+struct student{
     char name[30];
     int age;
-    enum Scores score;
+    enum score grade;
 };
 
-void addStudent(struct Student a[],int j){
-    char g;
-    if(j>=MAX_STUDENTS){
+void displayStudents(struct student stud[],int count){
+    if (count==0) {
+        printf("No students to display.\n");
+        return;
+    }
+
+    printf("List of students:\n");
+    int i;
+    for (i=0;i<count;i++) {
+        printf("Student %d\n",i + 1);
+        printf("Name: %s\n",stud[i].name);
+        printf("Age: %d\n",stud[i].age);
+
+        if (stud[i].grade==A) {
+            printf("Score: A\n");
+        }
+        else if (stud[i].grade==B) {
+            printf("Score: B\n");
+        }
+        else if (stud[i].grade==C) {
+            printf("Score: C\n");
+        }
+        else if (stud[i].grade==D) {
+            printf("Score: D\n");
+        }
+        else if (stud[i].grade==F) {
+            printf("Score: F\n");
+        }
+
+        printf("\n");
+    }
+}
+
+int findHighestScoringStudent(struct student stud[], int count){
+    if (count==0) {
+        return 0;
+    }
+
+    int highestScore=0;
+    int i;
+    for (i=0;i<count;i++) {
+        if(stud[i].grade<stud[highestScore].grade){
+            highestScore=i;
+        }
+    }
+
+    return highestScore;
+}
+
+int addStudent(struct student stud[],int count){
+    char grade[2];
+    if (count>=MAX_STUDENTS) {
         printf("Maximum number of students reached.\n");
+        return count;
+    }
+
+    scanf("%s",stud[count].name);
+    scanf("%d",&stud[count].age);
+    scanf("%s",grade);
+
+    if(strcmp(grade,"A")==0) {
+        stud[count].grade=A;
+    }
+    else if(strcmp(grade,"B")==0) {
+        stud[count].grade=B;
+    }
+    else if(strcmp(grade,"C")==0) {
+        stud[count].grade=C;
+    }
+    else if(strcmp(grade,"D")==0) {
+        stud[count].grade=D;
+    }
+    else if(strcmp(grade,"F")==0) {
+        stud[count].grade=F;
     }
     else{
-        scanf("%s",a[j].name);
-        scanf("%d",&a[j].age);
-        scanf(" %c",&g);
-        if(g=='A'){
-            a[j].score=0;
-        }
-        else if(g=='B'){
-            a[j].score=1;
-        }
-        else if(g=='C'){
-            a[j].score=2;
-        }
-        else if(g=='D'){
-            a[j].score=3;
-        }
-        else{
-            a[j].score=4;
-        }
-        printf("Student added successfully.\n");
+        stud[count].grade=F;
     }
+    //break;
+    count++;
+    printf("Student added successfully.\n");
+    return count;
 }
 
-void displayStudents(struct Student d[MAX_STUDENTS],int k){
-    if(k==0){
-        printf("No students to Display");
-    }
-    else{
-     printf("List of students:\n");
-     for(int i=0;i<MAX_STUDENTS;i++){
-         printf("Student %d\n",i+1);
-         printf("Name: %s\n",d[i].name);
-         printf("Age: %d\n",d[i].age);
-         if(d[i].score==0){
-             printf("Score: A \n");
-         }
-         else if(d[i].score==1){
-             printf("Score: B \n");
-         }
-         else if(d[i].score==2){
-             printf("Score: C \n");
-         }
-         else if(d[i].score==3){
-             printf("Score: D \n");
-         }
-         else{
-             printf("Score: F \n");
-         }
-         printf("\n");
-     }   
-    }
-}
-
-void findHighestScoringStudent(struct Student h[MAX_STUDENTS],int l){
-    if (l==0){
-        printf("No students to Display");
-    }
-    else{
-        printf("Highest-scoring student:\n");
-        int f=0;
-        for(int i=0;i<MAX_STUDENTS;i++){
-            if(h[i].score<h[f].score){
-                f=i;
-            }
-        }
-        printf("Name: %s\n",h[f].name);
-        printf("Age: %d\n",h[f].age);
-        if(h[f].score==0){
-                printf("Score: A \n");
-            }
-            else if(h[f].score==1){
-                printf("Score: B \n");
-            }
-            else if(h[f].score==2){
-                printf("Score: C \n");
-            }
-            else if(h[f].score==3){
-                printf("Score: D \n");
-            }
-            else{
-                printf("Score: F \n");
-            }
-        }
-    
-}
-
-int main(){
+int main() {
+    int count=0;
+    struct student stud[MAX_STUDENTS];
     int choice;
-    int i=0;
-    struct Student s[MAX_STUDENTS];
-    do{
-        scanf("%d",&choice);
-        switch(choice){
+
+    do {
+        scanf("%d", &choice);
+        switch(choice) {
             case 1:
-                addStudent(s,i);
-                i++;
+                count=addStudent(stud,count);
                 break;
             case 2:
-                displayStudents(s,i);
+                displayStudents(stud,count);
                 break;
-            case 3:
-                findHighestScoringStudent(s,i);
+            case 3:{
+                int val=findHighestScoringStudent(stud,count);
+                if(count!=0){
+                    printf("\nHighest-scoring student:\n");
+                    printf("Name: %s\n", stud[val].name);
+                    printf("Age: %d\n", stud[val].age);
+                    if (stud[val].grade==A) {
+                        printf("Score: A\n");
+                    }
+                    else if (stud[val].grade==B) {
+                        printf("Score: B\n");
+                    }
+                    else if (stud[val].grade==C) {
+                        printf("Score: C\n");
+                    }
+                    else if (stud[val].grade==D) {
+                        printf("Score: D\n");
+                    }
+                    else if (stud[val].grade==F) {
+                        printf("Score: F\n");
+                    }
+        //printf("\n");
+                }
+                else {
+                    printf("\nNo students to display.\n");
+                }
                 break;
+            }
             case 4:
-                printf("Exiting the program. Thank you for using our system!\n");
+                printf("\nExiting the program. Thank you for using our system!\n");
                 break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-                break;
-       }
-    } while (choice!=4);
+        }
+    } while(choice!= 4);
+
     return 0;
 }
